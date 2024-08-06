09554512d1b33dd46bd3a36cdfe8f1e1a99f41f5
DrRobot24 <dr.robot01@protonmail.com>
2024-08-06 17:36:24 +0200

    milestone n.1

--- src/main.jsx
+++ src/main.jsx
@@ -5,11 +5,15 @@ import './index.css'
 import { Amplify } from 'aws-amplify'
 import awsconfig from './aws-exports'
 import '@aws-amplify/ui-react/styles.css';  
+import { ThemeProvider } from "@aws-amplify/ui-react";
+import { studioTheme } from "./ui-components";
 
 Amplify.configure(awsconfig)
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={studioTheme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
 )
